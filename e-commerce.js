33544981b9ab37d5c5dcf4bf242c718b7a0921c1
nikhilsingh-dev2003 <nikhilsingh96160@gmail.com>
2026-03-08 11:30:08 +0530

    Delete e-commerce.js

--- e-commerce.js
+++
@@ -1,9 +0,0 @@
-let total=0;
-let count=0;
-function addTocart(price){
-    total+=price;
-    count++;
-    document.getElementById("total-price").innerText=total;
-    document.getElementById("cart-count").innerText=count;
-}
-
